4bcc200153d1f2c070566d553c1959ed551ecd6f
Kasper Vikström <kasper.vik@hotmail.com>
2025-03-28 21:47:34 +0100

    render SoundButton components from data and not from page

--- project-hex/app/page.tsx
+++ project-hex/app/page.tsx
@@ -1,19 +1,23 @@
 import MusicController from "./components/musiccontroller";
 import SoundButton from "./components/soundbutton";
+import { soundData } from "./data/sounds"; // Importera ljudspår
 import "./globals.css";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white p-4">
       <h1 className="text-3xl font-bold mb-6">Ljudspelare</h1>
-      <div className="grid grid-cols-2 gap-4 mb-6">
-        <SoundButton label="Nature" soundSrc="/sounds/power-of-nature.mp3" />
-        <SoundButton label="Witches" soundSrc="/sounds/witch-laugh.mp3" />
-        <SoundButton label="Mindreader" soundSrc="/sounds/mindreader.mp3" />
-        <SoundButton label="Ringbell" soundSrc="/sounds/ringbell.mp3" />
-        <SoundButton label="Thunder" soundSrc="/sounds/thunder.mp3" />
-        <SoundButton label="Dead-witch" soundSrc="/sounds/dead-witch.mp3" />
+
+      <div className="grid grid-cols-3 gap-4 mb-6">
+        {soundData.map((sound, index) => (
+          <SoundButton
+            key={index}
+            label={sound.label}
+            soundSrc={sound.soundSrc}
+          />
+        ))}
       </div>
+
       <MusicController />
     </div>
   );
