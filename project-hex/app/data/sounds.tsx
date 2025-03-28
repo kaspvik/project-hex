60170beae1b2f48f6722f127c9b7052b80bfb8ba
Kasper Vikström <kasper.vik@hotmail.com>
2025-03-28 21:46:33 +0100

    add sound data for audio effects

---
+++ project-hex/app/data/sounds.tsx
@@ -0,0 +1,8 @@
+export const soundData = [
+  { label: "Nature", soundSrc: "/sounds/power-of-nature.mp3" },
+  { label: "Witches", soundSrc: "/sounds/witch-laugh.mp3" },
+  { label: "Mindreader", soundSrc: "/sounds/mindreader.mp3" },
+  { label: "Ringbell", soundSrc: "/sounds/ringbell.mp3" },
+  { label: "Thunder", soundSrc: "/sounds/thunder.mp3" },
+  { label: "Dead-witch", soundSrc: "/sounds/dead-witch.mp3" },
+];
