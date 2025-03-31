f12acce92d70d9e928953d3fe9f4e2af0e3bb8ce
Kasper Vikström <kasper.vik@hotmail.com>
2025-03-31 19:34:47 +0200

    used scene activation instead of direct light control and simplify music playback logic

--- project-hex/app/components/musiccontroller.tsx
+++ project-hex/app/components/musiccontroller.tsx
@@ -4,39 +4,27 @@ import { useState } from "react";
 
 const HUE_BRIDGE_IP = "192.168.0.9";
 const HUE_API_KEY = "ppxtsTZYlLvVQYQRCFoCURWAPr3y4Zwml4buuGCt";
-const LIGHT_IDS = [4, 5, 6];
+const SCENE_ID_DAG = "d64HkvuifPFnM-cz";
+const SCENE_ID_NATT = "zIcCc5aWFKU8ETQd";
 const TRANSITION_TIME = 15; // 1.5 sekunder fade (Hue räknar i 100ms)
 
-const setLights = async (state: object) => {
-  LIGHT_IDS.forEach(async (id) => {
-    await fetch(
-      `http://${HUE_BRIDGE_IP}/api/${HUE_API_KEY}/lights/${id}/state`,
-      {
-        method: "PUT",
-        body: JSON.stringify(state),
-      }
-    );
+const activateScene = async (sceneId: string) => {
+  await fetch(`http://${HUE_BRIDGE_IP}/api/${HUE_API_KEY}/groups/0/action`, {
+    method: "PUT",
+    body: JSON.stringify({ scene: sceneId, transitiontime: 40 }),
   });
 };
 
 const MusicController: React.FC = () => {
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
 
-  const playMusicAndLights = (src: string, color: number[]) => {
+  const playMusic = (src: string) => {
     if (audio) {
       audio.pause();
     }
     const newAudio = new Audio(src);
-    newAudio.loop = true;
     newAudio.play();
     setAudio(newAudio);
-
-    setLights({
-      on: true,
-      bri: 254,
-      xy: color,
-      transitiontime: TRANSITION_TIME,
-    });
   };
 
   const stopMusic = () => {
@@ -48,33 +36,37 @@ const MusicController: React.FC = () => {
 
   return (
     <div className="grid grid-cols-2 gap-4 mt-6">
+      {/* Dag-knapp */}
       <button
-        onClick={() =>
-          playMusicAndLights("/sounds/new-dawn.mp3", [0.501, 0.415])
-        } // Varmt vitt
+        onClick={() => {
+          playMusic("/sounds/new-dawn.mp3");
+          activateScene(SCENE_ID_DAG);
+        }}
         className="flex flex-col items-center justify-center w-32 h-32 border-white border-2 bg-stone-900 hover:bg-stone-950 text-white font-bold shadow-lg transition-all">
         <span className="text-4xl">☀️</span>
         <span className="text-sm mt-2">Day</span>
       </button>
 
+      {/* Natt-knapp */}
       <button
-        onClick={() =>
-          playMusicAndLights("/sounds/night-time.mp3", [0.153, 0.048])
-        } // Blått
+        onClick={() => {
+          playMusic("/sounds/night-time.mp3");
+          activateScene(SCENE_ID_NATT);
+        }}
         className="flex flex-col items-center justify-center w-32 h-32 border-white border-2 bg-stone-900 hover:bg-stone-950 text-white font-bold shadow-lg transition-all">
         <span className="text-4xl">🌙</span>
         <span className="text-sm mt-2">Night</span>
       </button>
 
+      {/* Diskussion-knapp */}
       <button
-        onClick={() =>
-          playMusicAndLights("/sounds/discussion.mp3", [0.336, 0.364])
-        } // Neutralt vitt
-        className="flex flex-col items-center justify-center w-32 h-32 border-white border-2 bg-stone-900 hover:bg-stone-950 text-white font-bold shadow-lg transition-all">
+        onClick={() => playMusic("/sounds/discussion.mp3")}
+        className="flex flex-col items-center justify-center w-32 h-32 border-white border-2  bg-stone-900 hover:bg-stone-950 text-white font-bold shadow-lg transition-all">
         <span className="text-4xl">💬</span>
         <span className="text-sm mt-2">Discussion</span>
       </button>
 
+      {/* Stop-knapp */}
       <button
         onClick={stopMusic}
         className="flex flex-col items-center justify-center w-32 h-32 border-white border-2 bg-stone-900 hover:bg-stone-950 text-white font-bold shadow-lg transition-all">
